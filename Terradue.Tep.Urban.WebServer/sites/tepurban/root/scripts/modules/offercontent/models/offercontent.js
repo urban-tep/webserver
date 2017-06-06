589a8262e1d49f3944cf81421561cf0be5f4c5ee
Jenkins Hudson <jenkins@terradue.com>
2017-06-06 19:00:25 +0200

    "Release 1.0.4"

---
+++ Terradue.Tep.Urban.WebServer/sites/tepurban/root/scripts/modules/offercontent/models/offercontent.js
@@ -0,0 +1,16 @@
+﻿
+define(['can', 'config'], function(can, Config){
+	
+	return can.Model({
+		
+		sendSupportEmail: function(subject, body){
+			return $.post('/'+Config.api+'/support/email', {
+				subject: subject,
+				body: body
+			}, 'json');
+		},
+
+	}, {});
+	
+});
+
