489c9cafbde753443da98ebe0c38eaa5a1449cf4
Jenkins Hudson <jenkins@terradue.com>
2018-12-18 11:04:17 +0100

    "Release 2.3"

---
+++ Terradue.Tep.Urban.WebServer/Terradue/Tep/Urban/Controller/Auth/UTepLdapAuthenticationType.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web;
+using Terradue.Portal;
+
+namespace Terradue.Tep.Urban {
+
+    public class UTepLdapAuthenticationType : TepLdapAuthenticationType {
+        public UTepLdapAuthenticationType(IfyContext context) : base(context) { }
+
+        public override User GetUserProfile(IfyWebContext context, HttpRequest request = null, bool strict = false) {
+            var user = base.GetUserProfile(context, request, strict);
+
+            if (this.NewUserCreated) {
+                // new user has been created
+
+                // send information email to user
+                context.LogInfo(this, string.Format("New user created {0} - sending onboarding information", user.Username));
+
+                var subject = context.GetConfigValue("onboardingEmail_subject");
+                var body = context.GetConfigValue("onboardingEmail_body");
+                var username = !string.IsNullOrEmpty(user.FirstName) && !string.IsNullOrEmpty(user.LastName) ? user.FirstName + " " + user.LastName : user.Username;
+                body = body.Replace("$(USERNAME)", username);
+
+                try {
+                    context.SendMail(context.GetConfigValue("IT4ISupportEmail"), user.Email, subject, body);
+                } catch (Exception e) {
+                    context.LogError(this, "Unable to send email to add user to support system: " + e.Message);
+                }
+
+                // we add him to redmine support
+                context.LogInfo(this, string.Format("New user created {0} - adding to support system", user.Username));
+
+                subject = context.GetConfigValue("addtosupportEmail_subject");
+                body = context.GetConfigValue("addtosupportEmail_body");
+                body = body.Replace("$(USERNAME)", user.Username);
+
+                try {
+                    context.SendMail(user.Email, context.GetConfigValue("IT4ISupportEmail"), subject, body);
+                } catch (Exception e) {
+                    context.LogError(this, "Unable to send email to add user to support system: " + e.Message);
+                }
+            }
+
+            return user;
+        }
+    }
+}
---
+++ Terradue.Tep.Urban.WebServer/sites/tepurban/db/db-1.2.4#.sql
@@ -0,0 +1,5 @@
+USE $MAIN$;
+
+-- Update config...\
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('OfferContentEmailFrom', 'string', 'Offer Content Email From address', 'Offer Content Email From address', 'support.utep@it4i.cz', '1');
+-- RESULT
